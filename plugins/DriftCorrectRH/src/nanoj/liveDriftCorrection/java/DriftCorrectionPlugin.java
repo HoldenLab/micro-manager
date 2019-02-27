d0fc67077373bd96187c27bb8da594a3d71d35aa
Kevin <kevin.whitley@newcastle.ac.uk>
2019-02-27 14:56:00 +0000

    Updated DriftCorrectionRH code for MM2, made it actually load the plugin

--- plugins/DriftCorrectRH/src/nanoj/liveDriftCorrection/java/DriftCorrectionPlugin.java
+++ plugins/DriftCorrectRH/src/nanoj/liveDriftCorrection/java/DriftCorrectionPlugin.java
@@ -1,17 +1,22 @@
 package nanoj.liveDriftCorrection.java;
 
+import java.awt.event.WindowEvent;
+import mmcorej.CMMCore;
+import org.micromanager.Studio;
 import org.micromanager.internal.MMStudio;
 import org.scijava.plugin.SciJavaPlugin;
 //import org.micromanager.api.MMPlugin;
 //import org.micromanager.api.ScriptInterface;
 
-public class DriftCorrectionPlugin implements SciJavaPlugin {
+@org.scijava.plugin.Plugin(type = org.micromanager.MenuPlugin.class)
+public class DriftCorrectionPlugin implements org.micromanager.MenuPlugin, org.scijava.plugin.SciJavaPlugin {
     static DriftCorrectionGUI driftGui = DriftCorrectionGUI.INSTANCE;
 
     public static final String menuName = "NanoJ Online Drift Correction";
     public static final String tooltipDescription = "Asynchronous Image-Correlation-based 3D drift correction.";
     public static final String version = "0.5";
     public static final String copyright = "Copyright University College London, 2017";
+    private MMStudio app_;
 
     public void dispose() {}
 
@@ -32,4 +37,36 @@ public class DriftCorrectionPlugin implements SciJavaPlugin {
     public String getVersion() { return version; }
 
     public String getCopyright() { return copyright; }
+
+    @Override
+    public String getSubMenu() {
+        return "Beta";
+    }
+
+    @Override
+    public void onPluginSelected() {
+        if (driftGui != null) {
+         driftGui.setApp(app_);
+         driftGui.initiateThreads();
+         driftGui.show();
+      }
+      if (driftGui == null) {
+         //driftGui = new DriftCorrectionGUI();
+      }
+    }
+
+    @Override
+    public void setContext(Studio app) {
+        app_ = (MMStudio) app;
+    }
+
+    @Override
+    public String getName() {
+        return menuName;
+    }
+
+    @Override
+    public String getHelpText() {
+        return "Image-based cross-correlation developed by Ricardo Henriques lab";
+    }
 }
